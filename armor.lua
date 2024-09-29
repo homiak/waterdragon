af625c8a192731017dd018bb872f66c1ba5ccad4
Margo <margo@gmail.com>
2024-09-29 17:35:52 +0200

    armour

--- armor.lua
+++ armor.lua
@@ -75,6 +75,41 @@ armor:register_armor("waterdragon:boots_rare_water_draconic_steel", {
     damage_groups = {cracky=1, snappy=3, choppy=2, crumbly=1, level=30},
 })
 
+-- Scottish Dragon Draconic Armour
+
+armor:register_armor("waterdragon:helmet_scottish_draconic_steel", {
+    description = S("Scottish Water-Forged Draconic Steel Helmet"),
+    inventory_image = "waterdragon_inv_helmet_scottish_draconic_steel.png",
+    groups = {armor_head=1, armor_heal=30, armor_use=100,
+        physics_speed=0.5, physics_gravity=0.05, physics_jump=0.15, armor_fire=1},
+    armor_groups = {fleshy=130},
+    damage_groups = {cracky=1, snappy=3, choppy=2, crumbly=1, level=30},
+})
+armor:register_armor("waterdragon:chestplate_scottish_draconic_steel", {
+    description = S("Scottish Water-Forged Draconic Steel Chestplate"),
+    inventory_image = "waterdragon_inv_chestplate_scottish_draconic_steel.png",
+    groups = {armor_torso=1, armor_heal=30, armor_use=100,
+        physics_speed=0.5, physics_gravity=0.05, physics_jump=0.15, armor_fire=1},
+    armor_groups = {fleshy=130},
+    damage_groups = {cracky=1, snappy=3, choppy=2, crumbly=1, level=30},
+})
+armor:register_armor("waterdragon:leggings_scottish_draconic_steel", {
+    description = S("Scottish Water-Forged Draconic Steel Leggings"),
+    inventory_image = "waterdragon_inv_leggings_scottish_draconic_steel.png",
+    groups = {armor_legs=1, armor_heal=30, armor_use=100,
+        physics_speed=0.5, physics_gravity=0.05, physics_jump=0.15, armor_fire=1},
+    armor_groups = {fleshy=130},
+    damage_groups = {cracky=1, snappy=3, choppy=2, crumbly=1, level=30},
+})
+armor:register_armor("waterdragon:boots_scottish_draconic_steel", {
+    description = S("Scottish Water-Forged Draconic Steel Boots"),
+    inventory_image = "waterdragon_inv_boots_scottish_draconic_steel.png",
+    groups = {armor_feet=1, armor_heal=30, armor_use=100,
+        physics_speed=0.5, physics_gravity=0.05, physics_jump=0.15, armor_fire=1},
+    armor_groups = {fleshy=130},
+    damage_groups = {cracky=1, snappy=3, choppy=2, crumbly=1, level=30},
+})
+
 -- Pure Water Draconic Scale Armour
 
 armor:register_armor("waterdragon:leggings_pure_water_draconic", {
