e4eac20a9768bdd7b8dbd883ede80e4692db5263
Margo <margo@gmail.com>
2024-12-23 20:45:49 +0000

    shields

--- armor.lua
+++ armor.lua
@@ -330,39 +330,6 @@ armor:register_armor("waterdragon:boots_rare_water_draconic", {
 -- Shields --
 -------------
 
-local function reflect_damage(player, damage)
-    local attacker = player:get_meta():get_string("last_attacker")
-    if attacker and attacker ~= "" then
-        local attacker_obj = minetest.get_player_by_name(attacker)
-        if attacker_obj then
-            -- Отражаем 80% урона обратно атакующему
-            local reflected_damage = damage * 0.8
-            attacker_obj:set_hp(attacker_obj:get_hp() - reflected_damage)
-            -- Игрок получает только 20% от изначального урона
-            return damage * 0.2
-        end
-    end
-    -- Если атакующий не найден, игрок получает весь урон
-    return damage
-end
-
--- Регистрируем обработчик на урон
-minetest.register_on_player_hpchange(function(player, hp_change, reason)
-    if hp_change >= 0 or not player then
-        return hp_change -- Игнорируем положительные изменения HP
-    end
-
-    -- Проверяем, есть ли у игрока щит в слоте
-    local inv = player:get_inventory()
-    local shield_stack = inv:get_stack("armor_shield", 1)
-    if shield_stack:get_name() == "waterdragon:shield_pure_water_draconic_steel" then
-        -- Применяем отражение урона
-        return reflect_damage(player, math.abs(hp_change)) * -1
-    end
-
-    return hp_change -- Если щита нет, возвращаем исходный урон
-end)
-
 -- Draconic Steel Shields
 
 armor:register_armor("waterdragon:shield_pure_water_draconic_steel", {
@@ -371,9 +338,7 @@ armor:register_armor("waterdragon:shield_pure_water_draconic_steel", {
     groups = { armor_shield = 1, armor_heal = 30, armor_use = 100 },
     armor_groups = { fleshy = 100 },
     damage_groups = { cracky = 1, snappy = 3, choppy = 2, crumbly = 1, level = 30 },
-    on_damage_taken = function(player, index, stack, use, damage)
-        return reflect_damage(player, damage)
-    end,
+
 })
 
 armor:register_armor("waterdragon:shield_rare_water_draconic_steel", {
@@ -381,7 +346,8 @@ armor:register_armor("waterdragon:shield_rare_water_draconic_steel", {
     inventory_image = "waterdragon_inv_shield_rare_water_draconic_steel.png",
     groups = { armor_shield = 1, armor_heal = 40, armor_use = 100 },
     armor_groups = { fleshy = 120 },
-    damage_groups = { cracky = 1, snappy = 3, choppy = 2, crumbly = 1, level = 30 }
+    damage_groups = { cracky = 1, snappy = 3, choppy = 2, crumbly = 1, level = 30 },
+
 })
 
 -- Scale Shields
@@ -392,9 +358,8 @@ armor:register_armor("waterdragon:shield_pure_water_scales", {
     groups = { armor_shield = 1, armor_heal = 30, armor_use = 100 },
     armor_groups = { fleshy = 100 },
     damage_groups = { cracky = 1, snappy = 3, choppy = 2, crumbly = 1, level = 10 },
-    on_damage_taken = function(player, index, stack, use, damage)
-        return reflect_damage(player, damage)
-    end,
+
+
 })
 
 armor:register_armor("waterdragon:shield_rare_water_scales", {
@@ -403,7 +368,5 @@ armor:register_armor("waterdragon:shield_rare_water_scales", {
     groups = { armor_shield = 1, armor_heal = 40, armor_use = 100 },
     armor_groups = { fleshy = 120 },
     damage_groups = { cracky = 1, snappy = 3, choppy = 2, crumbly = 1, level = 15 },
-    on_damage_taken = function(player, index, stack, use, damage)
-        return reflect_damage(player, damage)
-    end,
+
 })
